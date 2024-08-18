9803fbda9575e34f46d9bdb91f461afd69369d97
SennyEiEi <166001826+SSENEIEI@users.noreply.github.com>
2024-08-18 19:01:46 +0700

    Update move.ino

--- 8sensor_pid/move.ino
+++ 8sensor_pid/move.ino
@@ -8,8 +8,8 @@ void timer(int sp, float Kp, float Kd, unsigned long durationMillis) {
 
 void runtocross(int sp, float Kp, float Kd) {
   while (true) {
-    if (sensor(0) > 50 && sensor(7) > 50) {
-      timer(sp, 0.01, 0, 100);
+    if (sensor(0) > 50 && sensor(5) > 50) {
+      timer(sp, 0, 1, 25);
       break;
     }
     Run_PID(sp, Kp, Kd);
@@ -41,7 +41,7 @@ void moveRobot(int speed1, int speed2) {
 void L() {
   moveRobot(-100, 85);
   delay(80);
-  while (analog(1) > 2000) {
+  while (analog(4) > 2000) {
     moveRobot(-25, 15);
   }
   moveRobot(100, -100);
@@ -53,7 +53,7 @@ void L() {
 void R() {
   moveRobot(85, -100);
   delay(85);
-  while (analog(4) > 2000) {
+  while (analog(3) > 2000) {
     moveRobot(15, -25);
   }
   moveRobot(-100, 100);
@@ -75,8 +75,96 @@ void U() {
 }
 
 void speed() {
-  for (int i = 50; i < 80; i++) {
-    Run_PID(i, 0.35, 0);
-    delay(20);
+  for (int i = 50; i < 100; i += 1) {
+    Run_PID(i, 0.05, 2);
   }
+}
+void checkReadline() {
+  int present_position = readline();
+  int setpoint = ((numsensor - 1) * 100) / 2;
+  int error = present_position - setpoint;
+  Serial.print("Readline: ");
+  Serial.println(error);
+}
+void viewSensorSerial() {
+  for (int i = 0; i < numsensor; i++) {
+    Serial.print("Sensor ");
+    Serial.print(i + 1);
+    Serial.print(": ");
+    Serial.print(sensor(i));
+    Serial.print(" Analog: ");
+    Serial.println(analog(i));
+  }
+  Serial.println("////////////////////");
+}
+void setCalibrate(int cal_round) {
+  for (int i = 0; i < numsensor; i++) {
+    set_min[i] = 1000;
+    set_max[i] = 0;
+  }
+
+  for (int round_count = 0; round_count < cal_round; round_count++) {
+    for (int i = 0; i < numsensor; i++) {
+      int sensorValue = analogRead(i);
+      set_max[i] = myMax(set_max[i], sensorValue);
+      set_min[i] = myMin(set_min[i], sensorValue);
+    }
+    delay(1);
+  }
+
+  tone(8, 2500, 400);  // Assuming the sound function is replaced with tone(pin, frequency, duration)
+
+  Serial.println("Calibration Complete");
+  Serial.println("set_max:");
+  for (int i = 0; i < numsensor; i++) {
+    Serial.print("Sensor ");
+    Serial.print(i + 1);
+    Serial.print(": ");
+    Serial.println(set_max[i]);
+  }
+
+  Serial.println("set_min:");
+  for (int i = 0; i < 4; i++) {
+    Serial.print("Sensor ");
+    Serial.print(i + 1);
+    Serial.print(": ");
+    Serial.println(set_min[i]);
+  }
+}
+int myMax(int a, int b) {
+  return (a > b) ? a : b;
+}
+
+int myMin(int a, int b) {
+  return (a < b) ? a : b;
+}
+void checkState(){
+  int present_position = readline();
+  int setpoint = ((numsensor - 1) * 100) / 2;
+  int error = present_position - setpoint;
+  if(error < 10 && error > -10){
+    Serial.print("FD");
+  }
+  if(error > 50){
+    Serial.print("R");
+  }
+  if(error < -50){
+    Serial.print("L");
+  }
+}
+void viewSensorOLED() {
+  oled.mode(1);
+  for (int i = 0; i < numsensor; i++) {
+    oled.text(i + 1, 0, "%d ", sensor(i));
+    oled.text(i + 1, 6, "%d   ", analog(i));
+  }
+  oled.show();
+}
+void checkReadlineOLED() {
+  oled.mode(2);
+  int present_position = readline();
+  int setpoint = ((numsensor - 1) * 100) / 2;
+  int error = present_position - setpoint;
+  oled.text(2,2,"   %d", error);
+  oled.show();
 }
